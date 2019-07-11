6bdde49a27ba6c27ad4bddff6ded15872ff22517
Tao Xu <taox@fb.com>
2019-07-10 23:39:40 -0700

    Update(auto commit)

--- tools/fix_xcode_copyright.py
+++ tools/fix_xcode_copyright.py
@@ -5,12 +5,17 @@ import fileinput
 """
 This script fixes the comment header for ".h" and ".m" files
 Usage:
-$python fixer (absolute path of the root folder)
+$python fix_xcode_copyright.py ${path_to_folder} ${exclude_path}
 """
 
 walk_dir = sys.argv[1]
+exclude_dir = sys.argv[2]
 file_data = ""
+print("- exclude: ", exclude_dir)
 for root, subdirs, files in os.walk(walk_dir):
+    print("- root: ",root)
+    if root.startswith(exclude_dir):
+        continue
     for filename in files:
         if filename.endswith(".h") or filename.endswith(".m"):
             filepath = os.path.join(root,filename)
