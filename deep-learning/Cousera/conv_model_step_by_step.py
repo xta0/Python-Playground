be9360fb838ae6ecb6178e1137d10d2e8b9f325b
Tao Xu <taox@fb.com>
2019-10-21 10:30:04 -0700

    Update(auto commit)

---
+++ deep-learning/Cousera/conv_model_step_by_step.py
@@ -0,0 +1,19 @@
+# Convolution_model_Step_by_Step_v2a 
+
+import numpy as np
+
+# GRADED FUNCTION: zero_pad
+def zero_pad(X, pad):
+    """
+    Pad with zeros all images of the dataset X. The padding is applied to the height and width of an image, 
+    as illustrated in Figure 1.
+    
+    Argument:
+    X -- python numpy array of shape (m, n_H, n_W, n_C) representing a batch of m images
+    pad -- integer, amount of padding around each image on vertical and horizontal dimensions
+    
+    Returns:
+    X_pad -- padded image of shape (m, n_H + 2*pad, n_W + 2*pad, n_C)
+    """
+    X_pad = np.pad(X, ((0,0),(pad, pad),(pad, pad),(0,0)), mode='constant', constant_values=(0,0);    
+    return X_pad
