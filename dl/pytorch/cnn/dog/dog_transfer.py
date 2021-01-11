6c85d4486b965e8805ac256681c5d2bd7c903f0d
Tao Xu <taox@fb.com>
2021-01-11 00:38:45 -0800

    Update(auto commit)

--- dl/pytorch/cnn/dog/dog_transfer.py
+++ dl/pytorch/cnn/dog/dog_transfer.py
@@ -1,10 +1,18 @@
 import os
+import numpy as np
 import torch
 from torchvision import datasets
+import torchvision.models as models
+import torch.nn as nn
 import torchvision.transforms as transforms
+
+# check if CUDA is available
+use_cuda = torch.cuda.is_available()
+
+### TODO: Write data loaders for training, validation, and test sets
+## Specify appropriate transforms, and batch_sizes
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-
 
 batch_size = 20
 num_workers = 0
@@ -17,80 +25,65 @@ transformer = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 ])
-train_data = datasets.ImageFolder('/data/dog_images/train', transform=transformer)
-valid_data = datasets.ImageFolder('/data/dog_images/test', transform=transformer)
-test_data = datasets.ImageFolder('/data/dog_images/valid', transform=transformer)
 
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, 
-                                           num_workers=num_workers, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, 
-                                          num_workers=num_workers, shuffle=True)
-valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=batch_size, 
-                                          num_workers=num_workers, shuffle=True)
-loaders_scratch = {
+transformer2 = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+train_data = datasets.ImageFolder('./dogImages/train', transform=transformer)
+valid_data = datasets.ImageFolder('./dogImages/test', transform=transformer2)
+test_data = datasets.ImageFolder('./dogImages/valid', transform=transformer2)
+
+train_loader = torch.utils.data.DataLoader(train_data,
+                                           batch_size=batch_size,
+                                           num_workers=num_workers,
+                                           shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data,
+                                          batch_size=16,
+                                          num_workers=num_workers,
+                                          shuffle=True)
+valid_loader = torch.utils.data.DataLoader(valid_data,
+                                           batch_size=16,
+                                           num_workers=num_workers,
+                                           shuffle=True)
+loaders_transfer = {
     "train": train_loader,
     "valid": valid_loader,
     "test": test_loader
 }
 
-# verify
-labels_nums = len(train_data.classes)
-print(labels_nums)
+model_transfer = models.vgg16(pretrained=True)
+print(model_transfer)
 
-tensor, label = next(iter(train_loader))
-print(tensor.shape)
+# freeze the parameters
+for param in model_transfer.features.parameters():
+    param.requires_grad = False
 
-import torch.nn as nn
-import torch.nn.functional as F
+# replace the last layer
+model_transfer.classifier[6] = nn.Linear(
+    model_transfer.classifier[6].in_features, 133, bias=True)
 
-# define the CNN architecture
-import torch.nn as nn
-import torch.nn.functional as F
-
-# define the CNN architecture
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.dropout = self.dropout = nn.Dropout(0.25)
-        self.fc1 = nn.Linear(28*28*64, 512)
-        self.fc2 = nn.Linear(512, 133)
-    
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.dropout(x.view(-1, 28*28*64))
-        x = self.dropout(F.relu(self.fc1(x)))
-        x = F.relu(self.fc2(x))
-        x = F.log_softmax(x, dim=1)
-        return x
-
-# instantiate the CNN
-model_scratch = Net()
-
-# move tensors to GPU if CUDA is available
 if use_cuda:
-    model_scratch.cuda()
+    model_transfer = model_transfer.cuda()
 
 import torch.optim as optim
 
-criterion_scratch = torch.nn.NLLLoss()
-optimizer_scratch = optim.SGD(model_scratch.parameters(), lr=0.01)
+criterion_transfer = torch.nn.CrossEntropyLoss()
+optimizer_transfer = optim.SGD(model_transfer.parameters(), lr=0.01)
+
 
 def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
     """returns trained model"""
     # initialize tracker for minimum validation loss
-    valid_loss_min = np.Inf 
-    
-    for epoch in range(1, n_epochs+1):
+    valid_loss_min = np.Inf
+
+    for epoch in range(1, n_epochs + 1):
         # initialize variables to monitor training and validation loss
         train_loss = 0.0
         valid_loss = 0.0
-        print("epoch: ", epoch)
         ###################
         # train the model #
         ###################
@@ -107,10 +100,10 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
             loss = criterion(Y, target)
             loss.backward()
             optimizer.step()
-#             train_loss = train_loss + ((1 / (batch_idx + 1)) * (loss.data - train_loss))
+            #             train_loss = train_loss + ((1 / (batch_idx + 1)) * (loss.data - train_loss))
             train_loss += loss.item() * data.size(0)
-            
-        ######################    
+
+        ######################
         # validate the model #
         ######################
         model.eval()
@@ -121,36 +114,70 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
             ## update the average validation loss
             Y = model(data)
             loss = criterion(Y, target)
-#             valid_loss = valid_loss + ((1 / (batch_idx + 1)) * (loss.data - train_loss))
+            #             valid_loss = valid_loss + ((1 / (batch_idx + 1)) * (loss.data - train_loss))
             valid_loss += loss.item() * data.size(0)
-                
+
         # calculate the average loss
         train_loss = train_loss / len(loaders['train'].dataset)
         valid_loss = valid_loss / len(loaders['valid'].dataset)
-            
-        # print training/validation statistics 
-        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
-            epoch, 
-            train_loss,
-            valid_loss
-            ))
-        
+
+        # print training/validation statistics
+        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.
+              format(epoch, train_loss, valid_loss))
+
         ## TODO: save the model if validation loss has decreased
         if valid_loss <= valid_loss_min:
             print(
                 'Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'
                 .format(valid_loss_min, valid_loss))
-            torch.save(model.state_dict(), 'model_cifar10.pt')
+            torch.save(model.state_dict(), 'model_scratch.pt')
             valid_loss_min = valid_loss
-            
+
     # return trained model
     return model
 
 
 # train the model
-print ("use_cuda: ", use_cuda)
-model_scratch = train(100, loaders_scratch, model_scratch, optimizer_scratch, 
-                      criterion_scratch, use_cuda, 'model_scratch.pth')
+print("use_cuda: ", use_cuda)
+model_transfer = train(20, loaders_transfer, model_transfer,
+                       optimizer_transfer, criterion_transfer, use_cuda,
+                       'model_transfer.pth')
 
 # load the model that got the best validation accuracy
-model_scratch.load_state_dict(torch.load('model_scratch.pth'))
+model_transfer.load_state_dict(torch.load('model_transfer.pth'))
+
+# test
+def test(loaders, model, criterion, use_cuda):
+    # monitor test loss and accuracy
+    test_loss = 0.
+    correct = 0.
+    total = 0.
+
+    model.eval()
+    for batch_idx, (data, target) in enumerate(loaders['test']):
+
+        # move to GPU
+        if use_cuda:
+            data, target = data.cuda(), target.cuda()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the loss
+        loss = criterion(output, target)
+        # update average test loss
+        test_loss = test_loss + ((1 / (batch_idx + 1)) *
+                                 (loss.data - test_loss))
+        # convert output probabilities to predicted class
+        pred = output.data.max(1, keepdim=True)[1]
+        # compare predictions to true label
+        correct += np.sum(
+            np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())
+        total += data.size(0)
+
+    print('Test Loss: {:.6f}\n'.format(test_loss))
+
+    print('\nTest Accuracy: %2d%% (%2d/%2d)' %
+          (100. * correct / total, correct, total))
+
+
+# call test function
+test(loaders_transfer, model_transfer, criterion_transfer, use_cuda)
