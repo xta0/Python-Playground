c51a13750bc8918b46ec39b1e10c1afe6b0a8d32
Tao Xu <taox@fb.com>
2024-12-12 23:39:53 -0800

    Update(auto commit)

---
+++ genai/coreml-llama/coreml-test.py
@@ -0,0 +1,53 @@
+import coremltools as ct
+
+# Load the model
+model = ct.models.MLModel("./BaselineLlamaForCausalLM.mlpackage/Data/com.apple.CoreML/model.mlmodel")
+print(model)
+"""
+input {
+  name: "inputIds"
+  type {
+    multiArrayType {
+      shape: 1
+      shape: 2048
+      dataType: INT32
+    }
+  }
+}
+input {
+  name: "attentionMask"
+  type {
+    multiArrayType {
+      shape: 1
+      shape: 2048
+      dataType: INT32
+    }
+  }
+}
+output {
+  name: "logits"
+  type {
+    multiArrayType {
+      shape: 1
+      shape: 2048
+      shape: 128256
+      dataType: FLOAT16
+    }
+  }
+}
+metadata {
+  userDefined {
+    key: "com.github.apple.coremltools.version"
+    value: "8.1"
+  }
+  userDefined {
+    key: "com.github.apple.coremltools.source"
+    value: "torch==1.12.0"
+  }
+  userDefined {
+    key: "com.github.apple.coremltools.source_dialect"
+    value: "TorchScript"
+  }
+}
+
+"""
