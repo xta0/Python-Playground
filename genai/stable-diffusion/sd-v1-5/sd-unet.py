d2973ce6510ff1f30e9d8a33d1cf06f549a71998
Tao Xu <taoxu.dev@gmail.com>
2025-03-24 09:38:06 -0700

    Update(auto commit)

--- genai/stable-diffusion/sd-v1-5/sd-unet.py
+++
@@ -1,8 +0,0 @@
-import torch
-from diffusers import UNet2DConditionModel
-
-unet = UNet2DConditionModel.from_pretrained(
-    "runwayml/stable-diffusion-v1-5",
-    subfolder = "unet",
-    torch_dtype = torch.float16
-).to("mps")
